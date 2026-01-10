bc8f1904bb90f7f88fb63212b4dc8a6e69b9004a
Jhonatas Magalhães <jhonatasbarbosa@yahoo.com.br>
2026-01-10 00:15:48 -0300

    remove modal de edicao e delecao

--- src/app/components/dashboard/statement-card/statement-card.component.ts
+++ src/app/components/dashboard/statement-card/statement-card.component.ts
@@ -1,13 +1,7 @@
 import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StatementListComponent } from '../statement-list/statement-list.component';
-import { EditTransactionModalComponent } from '../edit-transaction-modal/edit-transaction-modal.component';
-import { DeleteTransactionModalComponent } from '../delete-transaction-modal/delete-transaction-modal.component';
 import { Transaction } from '../../../shared/interfaces/account.interface';
-import { DeleteTransactionService } from '../../../services/delete-transaction/delete-transaction.service'; // Importar o serviço
-import { PutSaldoContaService } from '../../../services/put-saldo-conta/put-saldo-conta.service';
-import { InicioComponent } from '../../../pages/dashboard/inicio/inicio.component';
-import { InputComponent } from '../../common/input/input.component';
 import { DropdownComponent } from '../../common/dropdown/dropdown.component';
 import { FormsModule } from '@angular/forms';
 
@@ -17,8 +11,6 @@ import { FormsModule } from '@angular/forms';
   imports: [
     CommonModule,
     StatementListComponent,
-    EditTransactionModalComponent,
-    DeleteTransactionModalComponent,
     DropdownComponent,
     FormsModule
   ],
@@ -29,20 +21,11 @@ export class StatementCardComponent implements OnInit, OnChanges {
   @Input() transactions!: Transaction[] | null;
   @Input() accountId!: string | null;
 
-  isEditModalOpen: boolean = false;
-  transactionToEdit: Transaction | null = null;
-
-  isDeleteModalOpen: boolean = false;
-  transactionToDelete: Transaction | null = null;
-
   filteredTransactions: Transaction[] | null = null;
   searchQuery: string = '';
   filterType: string = '';
 
   constructor(
-    private deleteService: DeleteTransactionService,
-    private editService: PutSaldoContaService,
-    private parentComponent: InicioComponent
   ) { }
 
   ngOnInit(): void {
@@ -77,55 +60,5 @@ export class StatementCardComponent implements OnInit, OnChanges {
     }
 
     this.filteredTransactions = tempTransactions;
-  }
-
-  onEditRequest(transaction: Transaction): void {
-    this.transactionToEdit = transaction;
-    this.isEditModalOpen = true;
-  }
-
-  onDeleteRequest(id: string): void {
-    this.transactionToDelete = this.transactions?.find(t => t.id === id) || null;
-    if (this.transactionToDelete) {
-      this.isDeleteModalOpen = true;
-    }
-  }
-
-  closeEditModal(): void {
-    this.isEditModalOpen = false;
-    this.transactionToEdit = null;
-  }
-
-  closeDeleteModal(): void {
-    this.isDeleteModalOpen = false;
-    this.transactionToDelete = null;
-  }
-
-  onEditComplete(updatedTransaction: any): void {
-    if (updatedTransaction) {
-      this.editService.updateTransaction(updatedTransaction).subscribe({
-        next: () => {
-          this.parentComponent.fetchAccountData();
-          this.closeEditModal();
-        },
-        error: (err) => {
-          console.error('Erro ao atualizar a transação:', err);
-        }
-      });
-    }
-  }
-
-  onDeleteConfirm(id: string): void {
-    if (id) {
-      this.deleteService.deleteTransaction(id).subscribe({
-        next: () => {
-          this.parentComponent.fetchAccountData();
-          this.closeDeleteModal();
-        },
-        error: (err) => {
-          console.error('Erro ao deletar a transação:', err);
-        }
-      });
-    }
   }
 }
